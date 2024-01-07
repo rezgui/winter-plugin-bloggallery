80eb6b1a7c2d260b00dcf963f3bf0cfdf5cd3742
Yacine REZGUI <yacine.rezgui@gmail.com>
2024-01-08 00:12:55 +0100

    Update create_blog_gallery_field.php

--- updates/create_blog_gallery_field.php
+++ updates/create_blog_gallery_field.php
@@ -1,23 +1,23 @@
 <?php
 
-    namespace Martin\BlogGallery\Updates;
+    namespace Rezgui\BlogGallery\Updates;
 
     use Schema;
-    use October\Rain\Database\Updates\Migration;
+    use Winter\Storm\Database\Updates\Migration;
 
     class CreateBlogGalleryField extends Migration {
 
         public function up() {
-            Schema::table('rainlab_blog_posts', function ($table) {
+            Schema::table('winter_blog_posts', function ($table) {
                 $table->integer('rjgallery_id')->unsigned()->nullable();
                 $table->foreign('rjgallery_id')->references('id')->on('raviraj_rjgallery_galleries')->onDelete('set null');
             });
         }
 
         public function down() {
-            if(Schema::hasColumn('rainlab_blog_posts', 'rjgallery_id')) {
-                Schema::table('rainlab_blog_posts', function ($table) {
-                    $table->dropForeign('rainlab_blog_posts_rjgallery_id_foreign');
+            if(Schema::hasColumn('winter_blog_posts', 'rjgallery_id')) {
+                Schema::table('winter_blog_posts', function ($table) {
+                    $table->dropForeign('winter_blog_posts_rjgallery_id_foreign');
                     $table->dropColumn('rjgallery_id');
                 });
             }
