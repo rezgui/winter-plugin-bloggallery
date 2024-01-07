8445df6e2286ba70e82757d7ce4106b65e757f5b
Yacine REZGUI <yacine.rezgui@gmail.com>
2024-01-08 00:17:38 +0100

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,20 +1,20 @@
 <?php
 
-    namespace Martin\BlogGallery;
+    namespace Rezgui\BlogGallery;
 
     use System\Classes\PluginBase;
-    use RainLab\Blog\Controllers\Posts as PostsController;
-    use RainLab\Blog\Models\Post as PostModel;
+    use Winter\Blog\Controllers\Posts as PostsController;
+    use Winter\Blog\Models\Post as PostModel;
 
     class Plugin extends PluginBase {
 
-        public $require = ['RainLab.Blog', 'Raviraj.Rjgallery'];
+        public $require = ['Winter.Blog', 'Raviraj.Rjgallery'];
 
         public function pluginDetails() {
             return [
-                'name'        => 'martin.bloggallery::lang.plugin.name',
-                'description' => 'martin.bloggallery::lang.plugin.description',
-                'author'      => 'Martin M.',
+                'name'        => 'rezgui.bloggallery::lang.plugin.name',
+                'description' => 'rezgui.bloggallery::lang.plugin.description',
+                'author'      => 'Yacine REZGUI',
                 'icon'        => 'icon-image'
             ];
         }
@@ -29,10 +29,10 @@
                 if (!$model instanceof PostModel) return;
                 $form->addSecondaryTabFields([
                     'rjgallery' => [
-                        'label'       => 'martin.bloggallery::lang.form.label',
-                        'tab'         => 'rainlab.blog::lang.post.tab_manage',
+                        'label'       => 'rezgui.bloggallery::lang.form.label',
+                        'tab'         => 'rezgui.blog::lang.post.tab_manage',
                         'type'        => 'relation',
-                        'emptyOption' => 'martin.bloggallery::lang.form.empty'
+                        'emptyOption' => 'rezgui.bloggallery::lang.form.empty'
                     ]
                 ]);
             });
