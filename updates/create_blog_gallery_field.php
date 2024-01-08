f17245bb331ad75846f7ff994842b33812b98bd4
Yacine REZGUI <yacine.rezgui@gmail.com>
2024-01-08 01:31:29 +0100

    Update create_blog_gallery_field.php

--- updates/create_blog_gallery_field.php
+++ updates/create_blog_gallery_field.php
@@ -9,16 +9,16 @@
 
         public function up() {
             Schema::table('winter_blog_posts', function ($table) {
-                $table->integer('rjgallery_id')->unsigned()->nullable();
-                $table->foreign('rjgallery_id')->references('id')->on('raviraj_rjgallery_galleries')->onDelete('set null');
+                $table->integer('lightgallery_id')->unsigned()->nullable();
+                $table->foreign('lightgallery_id')->references('id')->on('hounddd_lightgallery_galleries')->onDelete('set null');
             });
         }
 
         public function down() {
-            if(Schema::hasColumn('winter_blog_posts', 'rjgallery_id')) {
+            if(Schema::hasColumn('winter_blog_posts', 'lightgallery_id')) {
                 Schema::table('winter_blog_posts', function ($table) {
-                    $table->dropForeign('winter_blog_posts_rjgallery_id_foreign');
-                    $table->dropColumn('rjgallery_id');
+                    $table->dropForeign('winter_blog_posts_lightgallery_id_foreign');
+                    $table->dropColumn('lightgallery_id');
                 });
             }
         }
